b3ca1733aa1ad69b5683e5900efbd0db52212118
Vadim Suleimanov <visulej2@mts.ru>
2017-04-04 14:56:13 +0300

    Texture render fix

--- templates/default/js/layout.js
+++ templates/default/js/layout.js
@@ -1,24 +1,29 @@
 (($, w, u) => {
 	
-	const textureSize = 256,
-		genereateTexture = (size) => {
+	const textureSize = 210,
+		icons = {
+			games: ['\uf1e3', '\uf11b', '\uf091', '\uf1e2', '\uf1b9'],
+			work: ['\uf0ac', '\uf19d', '\uf0c3', '\uf0b1', '\uf1ec'],
+			travel: ['\uf206', '\uf083', '\uf207', '\uf072', '\uf1ba', '\uf0f2'],
+		},
+		genereateTexture = (size, icons) => {
 			const el = document.createElement('canvas'),
-				ctx = el.getContext("2d");
+				ctx = el.getContext("2d"),
+				offsetY = 75;
+				
+			icons.sort((a, b) => {
+				return Math.random() > 0.5 ? 1 : -1;
+			});
 		
 			el.width = size * 2;
 			el.height = size;
 			
-			ctx.beginPath();
-			ctx.rect(0, 0, size, size);
-			ctx.fillStyle = "#3ebfd7";
-			ctx.fill();
-			
 			ctx.fillStyle = "rgba(256, 256, 256, .3)";
-			ctx.font = "10em FontAwesome";
-			ctx.fillText('\uf1e3', 0, 90);
-			ctx.fillText('\uf11b', size, 90);
-			ctx.fillText('\uf091', 0.5 * size, 0.5 * size + 90);
-			ctx.fillText('\uf1e2', 1.5 * size, 0.5 * size + 90);
+			ctx.font = "8em FontAwesome";
+			ctx.fillText(icons[0], 0, offsetY);
+			ctx.fillText(icons[1], size, offsetY);
+			ctx.fillText(icons[2], 0.5 * size, 0.5 * size + offsetY);
+			ctx.fillText(icons[3], 1.5 * size, 0.5 * size + offsetY);
 			
 			return el.toDataURL('image/png');
 		};
@@ -28,6 +33,6 @@
 			$('#navMenu').toggle();
 		});
 		
-		const jumbo = $('#jumbo').css('background-image', 'url(' + genereateTexture(textureSize) + ')');
+		const jumbo = $('#jumbo').css('background-image', 'url(' + genereateTexture(textureSize, icons.travel) + ')');
 	});	
 })(jQuery, window);
